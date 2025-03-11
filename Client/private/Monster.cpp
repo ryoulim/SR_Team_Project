0fe1a1fc244eb967875ff6f957ed6d078ec7f72b
Ryoul <luke0303@naver.com>
2025-03-11 13:13:34 +0900

    부모 클래스 추가 (복붙)

---
+++ Client/private/Monster.cpp
@@ -0,0 +1,90 @@
+#include "Monster.h"
+
+CMonster::CMonster(LPDIRECT3DDEVICE9 pGraphic_Device)
+	: CGameObject{ pGraphic_Device }
+{
+}
+
+CMonster::CMonster(const CMonster& Prototype)
+	: CGameObject(Prototype)
+{
+}
+
+HRESULT CMonster::Initialize_Prototype()
+{
+	return S_OK;
+}
+
+HRESULT CMonster::Initialize(void* pArg)
+{
+	if (FAILED(Ready_Components(pArg)))
+		return E_FAIL;
+
+	if (pArg != nullptr)
+	{
+		DESC* pDesc = static_cast<DESC*>(pArg);
+		m_pTransformCom->Set_State(CTransform::STATE_POSITION, pDesc->vInitPos);
+		m_pTransformCom->Scaling(pDesc->vScale);
+	}
+
+	return S_OK;
+}
+
+void CMonster::Priority_Update(_float fTimeDelta)
+{
+}
+
+void CMonster::Update(_float fTimeDelta)
+{
+}
+
+void CMonster::Late_Update(_float fTimeDelta)
+{
+	if (FAILED(m_pGameInstance->Add_RenderGroup(CRenderer::RG_NONBLEND, this)))
+		return;
+}
+
+HRESULT CMonster::Render()
+{
+	if (FAILED(m_pTransformCom->Bind_Resource()))
+		return E_FAIL;
+
+	if (FAILED(m_pTextureCom->Bind_Resource(static_cast<_uint>(m_fTextureNum))))
+		return E_FAIL;
+
+	if (FAILED(m_pVIBufferCom->Bind_Buffers()))
+		return E_FAIL;
+
+	if (FAILED(m_pVIBufferCom->Render()))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CMonster::Ready_Components(void* pArg)
+{
+	/* For.Com_Texture */
+	if (FAILED(__super::Add_Component(m_eLevelID, _wstring(TEXT("Prototype_Component_Texture_")) + m_szTextureID,
+		TEXT("Com_Texture"), reinterpret_cast<CComponent**>(&m_pTextureCom))))
+		return E_FAIL;
+
+	/* For.Com_VIBuffer */
+	if (FAILED(__super::Add_Component(LEVEL_STATIC, _wstring(TEXT("Prototype_Component_VIBuffer_")) + m_szBufferType,
+		TEXT("Com_VIBuffer"), reinterpret_cast<CComponent**>(&m_pVIBufferCom))))
+		return E_FAIL;
+
+	/* For.Com_Transform */
+	if (FAILED(__super::Add_Component(LEVEL_STATIC, TEXT("Prototype_Component_Transform"),
+		TEXT("Com_Transform"), reinterpret_cast<CComponent**>(&m_pTransformCom), pArg)))
+		return E_FAIL;
+
+	return S_OK;
+}
+void CMonster::Free()
+{
+	__super::Free();
+
+	Safe_Release(m_pVIBufferCom);
+	Safe_Release(m_pTextureCom);
+	Safe_Release(m_pTransformCom);
+}
---
+++ Client/private/Pawn.cpp
@@ -0,0 +1,90 @@
+#include "Pawn.h"
+
+CPawn::CPawn(LPDIRECT3DDEVICE9 pGraphic_Device)
+	: CGameObject{ pGraphic_Device }
+{
+}
+
+CPawn::CPawn(const CPawn& Prototype)
+	: CGameObject(Prototype)
+{
+}
+
+HRESULT CPawn::Initialize_Prototype()
+{
+	return S_OK;
+}
+
+HRESULT CPawn::Initialize(void* pArg)
+{
+	if (FAILED(Ready_Components(pArg)))
+		return E_FAIL;
+
+	if (pArg != nullptr)
+	{
+		DESC* pDesc = static_cast<DESC*>(pArg);
+		m_pTransformCom->Set_State(CTransform::STATE_POSITION, pDesc->vInitPos);
+		m_pTransformCom->Scaling(pDesc->vScale);
+	}
+
+	return S_OK;
+}
+
+void CPawn::Priority_Update(_float fTimeDelta)
+{
+}
+
+void CPawn::Update(_float fTimeDelta)
+{
+}
+
+void CPawn::Late_Update(_float fTimeDelta)
+{
+	if (FAILED(m_pGameInstance->Add_RenderGroup(CRenderer::RG_NONBLEND, this)))
+		return;
+}
+
+HRESULT CPawn::Render()
+{
+	if (FAILED(m_pTransformCom->Bind_Resource()))
+		return E_FAIL;
+
+	if (FAILED(m_pTextureCom->Bind_Resource(static_cast<_uint>(m_fTextureNum))))
+		return E_FAIL;
+
+	if (FAILED(m_pVIBufferCom->Bind_Buffers()))
+		return E_FAIL;
+
+	if (FAILED(m_pVIBufferCom->Render()))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CPawn::Ready_Components(void* pArg)
+{
+	/* For.Com_Texture */
+	if (FAILED(__super::Add_Component(m_eLevelID, _wstring(TEXT("Prototype_Component_Texture_")) + m_szTextureID,
+		TEXT("Com_Texture"), reinterpret_cast<CComponent**>(&m_pTextureCom))))
+		return E_FAIL;
+
+	/* For.Com_VIBuffer */
+	if (FAILED(__super::Add_Component(LEVEL_STATIC, _wstring(TEXT("Prototype_Component_VIBuffer_")) + m_szBufferType,
+		TEXT("Com_VIBuffer"), reinterpret_cast<CComponent**>(&m_pVIBufferCom))))
+		return E_FAIL;
+
+	/* For.Com_Transform */
+	if (FAILED(__super::Add_Component(LEVEL_STATIC, TEXT("Prototype_Component_Transform"),
+		TEXT("Com_Transform"), reinterpret_cast<CComponent**>(&m_pTransformCom), pArg)))
+		return E_FAIL;
+
+	return S_OK;
+}
+void CPawn::Free()
+{
+	__super::Free();
+
+	Safe_Release(m_pVIBufferCom);
+	Safe_Release(m_pTextureCom);
+	Safe_Release(m_pTransformCom);
+}
