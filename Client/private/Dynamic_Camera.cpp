8fc14e5b157616deaf0d6368a76ad818da0099a2
Ryoul <luke0303@naver.com>
2025-03-18 11:34:55 +0900

    병합

--- Client/private/Dynamic_Camera.cpp
+++ Client/private/Dynamic_Camera.cpp
@@ -49,17 +49,6 @@ void CDynamic_Camera::Priority_Update(_float fTimeDelta)
 #include "Mycube.h"
 EVENT CDynamic_Camera::Update(_float fTimeDelta)
 {
-	if (KEY_DOWN(VK_SPACE))
-	{
-		CMyCube::DESC CameraDesc{};
-		CameraDesc.vInitPos = *m_pTransformCom->Get_State(CTransform::STATE_POSITION);
-		CameraDesc.vScale = { 5.f,5.f,5.f };
-		CameraDesc.vLook = *m_pTransformCom->Get_State(CTransform::STATE_LOOK);
-		CameraDesc.fSpeedPerSec = 500.f;
-		m_pGameInstance->Active_Object(TEXT("ObjectPool_MyCube"), LEVEL_GAMEPLAY,
-			TEXT("Layer_Object"), &CameraDesc);
-	}
-
 	return __super::Update(fTimeDelta);
 }
 
