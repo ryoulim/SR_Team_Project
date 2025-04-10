0de157ff99a23a80c9d746906066db19da5d40e5
Ryoul <luke0303@naver.com>
2025-04-10 15:44:45 +0900

    준비중

--- Client/public/Level_RaceEnd.h
+++ Client/public/Level_RaceEnd.h
@@ -1,6 +1,35 @@
-#pragma once
-class CLevel_RaceEnd
-{
-};
+//#pragma once
+//
+//#include "Client_Defines.h"
+//#include "Level.h"
+//
+//BEGIN(Client)
+//
+//class CLevel_RaceEnd : public CLevel
+//{
+//private:
+//	CLevel_RaceEnd(LPDIRECT3DDEVICE9 pGraphic_Device);
+//	virtual ~CLevel_RaceEnd() = default;
+//
+//public:
+//	virtual HRESULT Initialize(class CLevelData* pLevelData);
+//	virtual void Update(_float fTimeDelta);
+//	virtual HRESULT Render();
+//
+//private:
+//	HRESULT Ready_Layer_Camera();
+//	HRESULT Ready_Layer_Terrain(const _wstring& strLayerTag);
+//	HRESULT Ready_Layer_Statue(const _wstring& strLayerTag);
+//	HRESULT Ready_Layer_Pawn(const _wstring& strLayerTag);
+//	HRESULT Ready_Layer_RaceBoss(const _wstring& strLayerTag);
+//	HRESULT Ready_Layer_UI(const _wstring& strLayerTag);
+//
+//public:
+//	static CLevel_RaceEnd* Create(LPDIRECT3DDEVICE9 pGraphic_Device, class CLevelData* pLevelData);
+//	virtual void Free()override;
+//};
+//
+//END
 
-// 뭔가 하려고 하는중
+
+///뭔가 하려고 하는중
